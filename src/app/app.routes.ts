48875cc25810f564763f006db23b5c17e25b8fa0
ChristopherLoisel <christo.loisel@gmail.com>
2024-06-03 15:23:22 +0200

    pathmatch

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { ConnexionComponent } from './components/connexion/connexion.component';
 import { TwittesComponent } from './components/twittes/twittes.component';
 
 export const routes: Routes = [
+	{ path: '', redirectTo: 'home', pathMatch: 'full' },
 	{ path: 'home', component: HomeComponent },
 	{ path: 'insciption', component: InscriptionComponent },
 	{ path: 'connexion', component: ConnexionComponent },
