5da20c887d3210b05f0c08382f9ccb96eef06240
ChristopherLoisel <christo.loisel@gmail.com>
2024-06-03 15:55:19 +0200

    directives

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector: 'app-root',
+	standalone: true,
+	imports: [RouterOutlet, RouterLink, FooterComponent], // ajout de RouterLink et RouterOutlet
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'twitter';
+	title = 'twitter';
 }
--- src/app/components/twittes/twittes.component.ts
+++ src/app/components/twittes/twittes.component.ts
@@ -1,12 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-twittes',
-  standalone: true,
-  imports: [],
-  templateUrl: './twittes.component.html',
-  styleUrl: './twittes.component.css'
+	selector: 'app-twittes',
+	standalone: true,
+	imports: [CommonModule],
+	templateUrl: './twittes.component.html',
+	styleUrl: './twittes.component.css'
 })
 export class TwittesComponent {
 
+	constructor() { }
+
+	isConnected = true;
+
+
+	tweetList = [
+		{
+			id: 1,
+			username: 'User1',
+			content: 'Content1',
+		},
+		{
+			id: 2,
+			username: 'User2',
+			content: 'Content2',
+		},
+		{
+			id: 3,
+			username: 'User3',
+			content: 'Content3',
+		},
+	];
 }
