96fb9094d99ad7545dc0e17afe7fe0204b081224
ChristopherLoisel <christo.loisel@gmail.com>
2024-06-03 14:58:35 +0200

    ok

---
+++ src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { InscriptionComponent } from './components/inscription/inscription.component';
+import { ConnexionComponent } from './components/connexion/connexion.component';
+import { TwittesComponent } from './components/twittes/twittes.component';
+
+export const routes: Routes = [
+	{ path: 'home', component: HomeComponent },
+	{ path: 'insciption', component: InscriptionComponent },
+	{ path: 'connexion', component: ConnexionComponent },
+	{ path: 'tweet', component: TwittesComponent }
+];
